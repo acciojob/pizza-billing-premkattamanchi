089cbc1e4b1d9a1e29a62773fc22011de758e18c
prem <premkattamanchi@gmail.com>
2023-01-19 11:29:06 +0530

    code created

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,9 +1,14 @@
 package com.driver;
 
 public class DeluxePizza extends Pizza {
-
-    public DeluxePizza(Boolean isVeg) {
+     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+         super.extraCheese=80;
+         if(isVeg==true){
+             super.extraToppings=70;
+         }
+         else
+             super.extraToppings=120;
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,16 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+     protected int extraCheese=0;
+     protected int extraToppings=0;
+    private int paperBag=0;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg)
+            this.price=300;
+        else
+            this.price=400;
     }
 
     public int getPrice(){
@@ -17,18 +23,28 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(this.extraCheese==0)
+            extraCheese=80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(this.extraToppings==0 && isVeg==true)
+            extraToppings=70;
+        else if(this.extraToppings==0 && isVeg==false)
+            extraToppings=120;
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(this.paperBag==0)
+            paperBag=20;
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        this.bill="Base Price Of The Pizza: "+this.price+"\n"+"Extra Cheese Added: "+this.extraCheese+"\n"
+                +"Extra Toppings Added: "+this.extraToppings+"\n"+"Paperbag Added: "+this.paperBag+"\n"+"Total Price: "+
+                (this.price+this.extraCheese+this.extraToppings+this.paperBag);
+    return this.bill;
     }
 }
