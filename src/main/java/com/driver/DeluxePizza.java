239f408a898dea81139db8cfa6d87b6bb27382c5
prem <premkattamanchi@gmail.com>
2023-01-24 08:11:14 +0530

    modified

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,12 +4,7 @@ public class DeluxePizza extends Pizza {
      public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-         super.extraCheese=80;
-         if(isVeg==true){
-             super.extraToppings=70;
-         }
-         else
-             super.extraToppings=120;
          addExtraCheese();
+         addExtraToppings();
     }
 }
